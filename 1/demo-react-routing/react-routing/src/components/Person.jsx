189ef4cc1a8cfb712f5893dca550e648f3cdd032
fiorella frini <fiorella_frini@hotmail.com>
2023-10-28 15:02:08 +0200

    person

---
+++ 1/demo-react-routing/react-routing/src/components/Person.jsx
@@ -0,0 +1,9 @@
+const Person = ({ id, name }) => {
+  return (
+    <div>
+      <h3>Name: {name}</h3>
+      <h3>Id: {id}</h3>
+    </div>
+  );
+};
+export default Person;
--- 1/demo-react-routing/react-routing/src/components/Profile.jsx
+++ 1/demo-react-routing/react-routing/src/components/Profile.jsx
@@ -1,10 +1,36 @@
-const Profile = ()=>{
-    return(
-        <div>
-            <h1>Profile</h1>
-            <p>mucha dataaa</p>
-        </div>
-    )
-}
+import { useEffect, useState } from "react";
+import axios from "axios";
+import Person from "./Person";
+
+const Profile = () => {
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    axios("https://jsonplaceholder.typicode.com/users")
+      .then(({ data }) => {
+        setUsers([...users, ...data]);
+      })
+      .catch(() => {
+        console.log("se rompio");
+      });
+  }, []);
+
+  return (
+    <div>
+      <h1>Profile</h1>
+      <p>mucha dataaa</p>
+
+      {users?.map((prop) => {
+        return (
+          <Person
+            key={prop.id}
+            id={prop.id}
+            name={prop.name}
+          />
+        );
+      })}
+    </div>
+  );
+};
 
 export default Profile;
