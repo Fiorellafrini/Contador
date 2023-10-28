731ce50a86a3d88d53813b2bcf22017262eec1e8
fiorella frini <fiorella_frini@hotmail.com>
2023-10-28 10:53:26 +0200

    contador con componente de clase

---
+++ 1/demo-react/src/App.jsx
@@ -0,0 +1,17 @@
+// import { useState } from 'react'
+import './App.css'
+import Contador from './assets/components/Contador'
+
+function App() {
+  // const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div>
+   <Contador />
+      </div>
+    </>
+  )
+}
+
+export default App
---
+++ 1/demo-react/src/assets/components/Contador.jsx
@@ -0,0 +1,33 @@
+import React from "react";
+
+//COMPONENTE DE CLASE, MANEJO DE ESTADOS
+
+class Contador extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      counter: 0,
+      saludo: "Bienvenidos",
+    };
+  }
+
+  click = () => {
+    this.setState({counter: this.state.counter +1, saludo: "Ya sume!"}, ()=>{
+        console.log(this.state.counter); //hago la callback para que el consollog coincida con lo renderizado. poruqe si no el console muestra el valor anterior.
+
+    }) //con el setstate ejecuto el estado
+  };
+
+  render() {
+    return (
+      <div>
+        <h1>CLASS COMPONENT</h1>
+        <h3>{this.state.saludo}</h3>
+        <p>{this.state.counter}</p>
+
+        <button onClick={this.click}>Aumentar</button>
+      </div>
+    );
+  }
+}
+export default Contador;
